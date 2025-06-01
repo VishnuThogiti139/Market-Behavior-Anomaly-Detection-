ab931df77fa0f55fa9d040236eed9b661e692b44
VishnuThogiti139 <thogitivishnu1079@gmail.com>
2025-06-01 13:37:59 -0400

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 from datetime import datetime, timedelta
 
-def fetch_stock_data(symbol="AAPL", years_back=5):
+def fetch_stock_data(symbol="", years_back=5):
     end_date = datetime.today()
     start_date = end_date - timedelta(days=years_back * 365)
     df = yf.download(symbol, start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
